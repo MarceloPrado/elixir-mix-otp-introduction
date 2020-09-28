6f21a5f0f3ac76ba073ab69c385342fa5644654f
Marcelo Prado <marceloterreiroprado@gmail.com>
2020-09-28 09:08:40 -0300

    fix/test name typo

--- test/bucket_test.exs
+++ test/bucket_test.exs
@@ -13,7 +13,7 @@ defmodule KV.BucketTest do
     assert KV.Bucket.get(bucket, "milk") == 3
   end
 
-  test "delete value by key and returns it", %{bucket: bucket} do
+  test "deletes value by key and returns it", %{bucket: bucket} do
     KV.Bucket.put(bucket, "milk", 42)
     assert KV.Bucket.get(bucket, "milk") == 42
 
